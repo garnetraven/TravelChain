bb006cf9374b0da3f0391be5bb22ae7a6e6f383e
garnetraven <corbinchristopher1@gmail.com>
2024-03-31 12:58:24 -0700

    init commit

--- src/components/homepage.js
+++ src/components/homepage.js
@@ -7,6 +7,9 @@ const Homepage = () => {
     <div className="container mt-5">
       <div className="homepage-content">
         <img src="/logo.png" alt="Logo" className="logo" /> {/* Apply styling for the logo */}
+        <h2>Store, track, and enjoy your photos with the security of TravelChain.</h2>
+        <h3>All with the assurance and security of blockchain.</h3>
+        <p></p>
         <Link to="/signin" className="btn btn-primary sign-in-btn">Sign in</Link> {/* Apply styling for the sign-in button */}
       </div>
     </div>
